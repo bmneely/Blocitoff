60acbb66f58e9aea48ec82c8c135135362f8ad95
boumsam <sam_boumeester41@hotmail.com>
2016-12-02 01:15:05 +0000

    production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  #config.action_mailer.default_url_options = { host: ‘https://blocitoff-boumsam.c9users.io/’ }
+  config.action_mailer.default_url_options = { host: 'https://secure-refuge-78855.herokuapp.com' }
   
   config.action_mailer.delivery_method = :smtp
 

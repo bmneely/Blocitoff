4149526da0ef0e25ede43e7b6da63edbc5283f79
boumsam <sam_boumeester41@hotmail.com>
2016-12-20 01:36:54 +0000

    Production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://secure-refuge-78855.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://polar-crag-97312.herokuapp.com' }
   
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.perform_deliveries = true
 end

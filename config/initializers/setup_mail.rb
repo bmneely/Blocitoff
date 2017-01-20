c20d9398e7497d766e891ceb9a82267cfbe755b6
boumsam <sam_boumeester41@hotmail.com>
2017-01-20 01:33:53 +0000

    email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,7 +2,8 @@ if Rails.env.development? || Rails.env.production?
     ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
       address:        'smtp.sendgrid.net',
-      port:           '2525',
+      #port:           '2525',
+      port:            587,
       authentication: :plain,
       user_name:      ENV['MAIL_USERNAME'],
       password:       ENV['MAIL_PASSWORD'],
